9149b820995c64a2e5e42ea0d24507443ccf45be
Emily Chidester <erollinschidester@gmail.com>
2024-04-22 08:17:15 -0400

    Prompt error handling
    Implemented error handling within the "Pick Grid Size" button click
    to only allow for numbers between 1-100 to be used. Otherwise, the
    application will continue to prompt for valid entry.

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-const numRows = prompt("How many squares?");
+document.querySelector("button").addEventListener("click", function () {
+  let numRows = parseInt(prompt("Enter a grid size between 1-100:"));
 
-for (let i = 0; i < numRows; i++) {
-  const containerDiv = document.querySelector(".container");
-  const newRow = document.createElement("div");
-  newRow.className = `row-${i} row`;
-  containerDiv.appendChild(newRow);
-
-  for (let j = 0; j < numRows; j++) {
-    const rowDiv = document.querySelector(`.row-${i}`);
-    const newTile = document.createElement("div");
-    newTile.className = `tile`;
-    rowDiv.appendChild(newTile);
-    console.log(`new tile ${j}`);
+  while (!numRows || numRows > 100 || numRows < 1) {
+    numRows = parseInt(prompt("Enter a grid size between 1-100:"));
   }
-}
 
-const tiles = document.querySelectorAll(".tile");
+  createGrid(numRows);
+});
 
-tiles.forEach((tile, index) =>
-  tile.addEventListener("mouseover", function () {
-    tile.classList.add("colored");
-  })
-);
+const createGrid = function (numRows = 10) {
+  // Variables
+  const containerDiv = document.querySelector(".container");
+
+  // Drop previous Grid
+  containerDiv.innerHTML = "";
+
+  // Create numRows # of rows
+  for (let i = 0; i < numRows; i++) {
+    const newRow = document.createElement("div");
+    newRow.className = `row-${i} row`;
+    containerDiv.appendChild(newRow);
+
+    // Create numRows # of tiles within each row
+    for (let j = 0; j < numRows; j++) {
+      const rowDiv = document.querySelector(`.row-${i}`);
+      const newTile = document.createElement("div");
+      newTile.className = `tile`;
+      rowDiv.appendChild(newTile);
+    }
+  }
+
+  // Add Event Listeners to every tile to identify when the tile is moused over
+  const tiles = document.querySelectorAll(".tile");
+
+  tiles.forEach((tile, index) =>
+    tile.addEventListener("mouseover", function () {
+      tile.classList.add("colored");
+    })
+  );
+};
+
+createGrid();
